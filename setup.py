951e9c0ed74c76623f8c18da8b19d4919c7a21fc
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2024-10-18 03:08:03 +0530

    setup_file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#Used to avoid import package error
+from setuptools import find_packages,setup
+
+setup(
+    name='AetherCare_ChatBot',
+    version='0.0.0',
+    author='volatile_ARG',
+    author_email='aadityarajgupta333@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
